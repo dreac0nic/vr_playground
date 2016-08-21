6ce0b6621c9e2456985bb656b934cd252252edcc
spenser <spenser.bray@gmail.com>
2016-08-20 19:34:41 -0700

    Build a simple memory-helping script to remove unneccessary balls.

---
+++ Assets/scripts/BallKiller.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallKiller : MonoBehaviour
+{
+  public float KillHeight = -1000.0f;
+  
+  protected void Update() {
+    if(this.transform.position.y < KillHeight) {
+      GameObject.Destroy(this.gameObject);
+    }
+  }
+}
