6ddcc9a5cb39fbd724a3237288704af184eb738f
spenser <spenser.bray@gmail.com>
2016-08-20 19:36:15 -0700

    Use a filter script to build a simple spawner.

---
+++ Assets/scripts/SimpleLever.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleLever : MonoBehaviour
+{
+  public GameObject SpawnPrefab;
+  public Transform SpawnAnchor;
+  
+  public void Impulse() {
+    Instantiate(SpawnPrefab, SpawnAnchor.position, Quaternion.identity);
+  }
+}
