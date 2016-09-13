8fbf1811aad1d6fa47317e0f41e9982a9f5711f8
spenser <spenser.bray@gmail.com>
2016-09-12 23:58:49 -0700

    Build a simple collector function

--- Assets/scripts/VRInteractor.cs
+++ Assets/scripts/VRInteractor.cs
@@ -57,5 +57,26 @@ public class VRInteractor : MonoBehaviour
 
     return false;
   }
+
+  protected VRInteractable findInteractable() {
+    double closest_distance = System.Double.MaxValue;
+    Collider[] colliders = Physics.OverlapSphere(this.transform.position, InteractRadius, InteractFilter, QueryTriggerInteraction.Collider);
+    VRInteractable target_interactable = null;
+
+    // Find the closest interactable object
+    foreach(Collider possible_interactable in colliders) {
+      VRInteractable marked_interactable = possible_interactable.GetComponentInParrent<VRInteractable>();
+
+      if(marked_interactable && marked_interactable.IsInteractable(this)) {
+	double distance = (interactable.transform.position - this.transform.position).sqrMagnitude;
+
+	if(distance < closest_distance) {
+	  closest_distance = distance;
+	  target_interactable = marked_interactable;
+	}
+      }
+    }
+    
+    return target_interactable;
   }
 }
